56d929b869cb78d9ea9b51bca426415f5d8a7f34
benarof <benjamin.lauvergeon@sfr.fr>
2020-01-10 09:22:35 +0100

    rajout LD
    voir dossier "Level_Design"

---
+++ Assets/Level_Design/script_trials/epreuves/Lever.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    [SerializeField] private GameObject _leverBrok = null;
+    [SerializeField] private GameObject _leverRepear = null;
+    [SerializeField] private GameObject _collider1 = null;
+    [SerializeField] private GameObject _collider2 = null;
+    private bool _activate1 = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _activate1 = !_activate1;
+        _leverBrok.SetActive(!_activate1);
+        _collider1.SetActive(!_activate1);
+        _leverRepear.SetActive(_activate1);
+        _collider2.SetActive(_activate1);
+
+    }
+}
---
+++ Assets/Level_Design/script_trials/epreuves/button.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class button : MonoBehaviour
+{
+    [SerializeField] private GameObject _door1 = null;
+    [SerializeField] private GameObject _door2 = null;
+    private bool _activate1 = false;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _activate1 = !_activate1;
+        _door1.SetActive(_activate1);
+        _door2.SetActive(!_activate1);
+    }
+}
