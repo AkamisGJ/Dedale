5570678d5c4349ac3d474003ffcbf3f8a6bf0740
benarof <benjamin.lauvergeon@sfr.fr>
2020-02-11 13:58:53 +0100

    Update scene relation amoureuse
    Add collider

---
+++ Assets/Level_Design/script_trials/Portals/TeleporterIKEA.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterIKEA : MonoBehaviour
+{
+    [SerializeField] private Transform _otherPos = null;
+    private Transform _player;
+    [SerializeField] private bool _isTp = false;
+    private void Start()
+    {
+        _player = PlayerManager.Instance.PlayerController.transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(_isTp == false && other.GetComponent<PlayerAgentController>() != null)
+        {
+             _player.position = _otherPos.position;
+            _isTp = true;
+        }
+    }
+}
---
+++ Assets/Level_Design/script_trials/epreuves/relation_amoureuse/telepot.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class telepot : MonoBehaviour
+{
+
+    [SerializeField] private Transform _player = null;
+    [SerializeField] private Transform _reciever = null;
+
+    private bool playerIsOverlapping = false;
+
+
+    private void Start()
+    {
+        _player = PlayerManager.Instance.PlayerController.transform;
+    }
+    void Update()
+    {
+        if (playerIsOverlapping == true)
+        {
+            Vector3 portalToPlayer = _player.localPosition - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if (dotProduct < 0f)
+            {
+                float rotationDiff = -Quaternion.Angle(transform.rotation, _reciever.rotation);
+                rotationDiff += 180;
+                _player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                _player.position = _reciever.position + positionOffset;
+
+                playerIsOverlapping = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerIsOverlapping = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        playerIsOverlapping = false;
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
+    }
+
+}
