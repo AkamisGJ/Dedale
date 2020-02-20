d96de1164dc7abc70ee5b3764f9d0a0e6f0eabf5
ValorisFR <valoris@hotmail.fr>
2020-02-20 09:36:42 +0100

    Merge from scene tuto

---
+++ Assets/Scripts/Trigger_Event_Once.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.Wrappers;
+
+public class Trigger_Event_Once : TriggerEvent
+{
+    protected override void OnTriggerEnter(Collider other)
+        {
+            if (!IsInTagMask(other.tag)) return;
+            onTriggerEnter.Invoke(other.gameObject);
+            Destroy(this);
+        }
+}
