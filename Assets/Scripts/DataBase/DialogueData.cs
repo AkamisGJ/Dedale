6ccfaaf33a7b68766f1b427cb79d0003a0e2b7b4
ValorisFR <valoris@hotmail.fr>
2020-03-19 12:22:55 +0100

    begin dialog system custom

---
+++ Assets/Scripts/DataBase/DialogueData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using FMODUnity;
+
+[CreateAssetMenu(fileName = "DialogueData", menuName = "DataBase/DIalogueData")]
+public class DialogueData : ScriptableObject
+{
+    #region Fields
+    [SerializeField] private string[] _dialogue = null;
+    [EventRef]
+    [SerializeField] private string _event = "";
+    #endregion Fields
+
+    #region Properties
+    public string[] Dialogue { get { return _dialogue; } set { _dialogue = value; } }
+    public string AudioClip { get { return _event; } set { _event = value; } }
+    #endregion Properties
+}
