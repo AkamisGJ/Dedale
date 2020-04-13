17e7c5343f2c0467d40fcf7d90a79baa82a6da79
Akamis <mrtetex@gmail.com>
2020-04-13 15:22:12 +0200

    Reorder Script and create Water Shader

---
+++ Assets/Scripts/SliderWeightVolume.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SliderWeightVolume : MonoBehaviour
+{
+    [SerializeField] private float _timeToBlend = 1f;
+    private float _increasseAmout = 0f;
+    private bool blending = false;
+    private Volume _volume;
+    
+    void Start()
+    {
+        _volume = GetComponent<Volume>();
+        _increasseAmout = Time.deltaTime / _timeToBlend;
+    }
+
+    public void StartBlending()
+    {
+        blending = true;
+    }
+
+    private void Update()
+    {
+        if (blending)
+        {
+            _volume.weight += _increasseAmout;
+
+            if(_volume.weight > 1)
+            {
+                blending = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/cailloux_vollant.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class cailloux_vollant : MonoBehaviour
+{
+    //Position
+    [InfoBox("Put the curve between time -1 and 1")]
+    [SerializeField] AnimationCurve curve;
+    private Vector3 initial_position;
+    private Vector3 new_position = Vector3.zero;
+    float time;
+    float random_startTime = 0f;
+
+    //Rotation
+    private Quaternion initialRotation;
+    private Quaternion newRotation = Quaternion.identity;
+    private float lerpRotationValue = 0f; 
+    [SerializeField] private float speedRotation = 0.2f;
+
+
+    void Start()
+    {
+        initial_position = transform.position;
+        initialRotation = transform.rotation;
+        random_startTime = Random.Range(-1f, 1f);
+    }
+
+
+    void Update()
+    {
+        MovePosition();
+        ChangeRotation();
+    }
+
+    void ChangeRotation(){
+        if(transform.rotation == newRotation || lerpRotationValue > 1f){
+            newRotation = Random.rotation;
+            initialRotation = transform.rotation;
+            lerpRotationValue = 0f;
+        }
+        lerpRotationValue += Time.deltaTime * speedRotation;
+        transform.rotation = Quaternion.Lerp(initialRotation, newRotation, lerpRotationValue);
+    }
+
+    void MovePosition(){
+        time = Mathf.Sin(Time.time + random_startTime);
+        new_position = initial_position;
+        new_position.y += curve.Evaluate(time);
+        transform.position = new_position;
+    }
+}
