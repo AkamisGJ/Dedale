019b112cc2e225793351d60fccfd57d4df6dc3cf
benarof <benjamin.lauvergeon@sfr.fr>
2020-04-15 16:19:21 +0200

    Level Hopital BenL structure principal

---
+++ Assets/Level_Design/script_trials/Hopital/Anim_Hopital.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Anim_Hopital : MonoBehaviour
+{
+    [SerializeField] private Animator _animation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _animation.SetBool("down", true);
+        }
+        else
+        {
+            _animation.SetBool("down", false);
+        }
+    }
+}
+
