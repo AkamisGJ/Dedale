c61010cb331be2893e941bce9df56fc723b6498a
Akamis <mrtetex@gmail.com>
2020-04-16 11:55:44 +0200

    Add pelluche Zoe code + remove old Hightlight system

---
+++ Assets/SwapEtiquette.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapEtiquette : MonoBehaviour
+{
+    [SerializeField] private Material _etiquette = null;
+    [SerializeField] private float lerpDuration = 1f;
+    private float lerpValue = 0f;
+
+    public void StartLerping(){
+        GameLoopManager.Instance.GameLoopPlayer += OnUpdate;
+    }
+
+    private void OnUpdate(){
+        _etiquette.SetFloat("Vector1_9532F5BD", lerpValue);
+        lerpValue += Time.deltaTime / lerpDuration;
+        if(lerpValue >= 1f){
+            GameLoopManager.Instance.GameLoopPlayer -= OnUpdate;
+        }
+    }
+}
