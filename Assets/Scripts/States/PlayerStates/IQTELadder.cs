91f18540a219985c9d52919643126418e302efef
ValorisFR <valoris@hotmail.fr>
2020-05-02 11:18:35 +0200

    update door

--- Assets/Scripts/States/PlayerStates/IQTELadder.cs
+++ Assets/Scripts/States/PlayerStates/IQTELadder.cs
@@ -9,15 +9,21 @@ public class IQTELadder : IPlayerState
     private Vector3 _directionHorizontal = Vector3.zero;
     private Vector3 _orientation = Vector3.zero;
     private bool _isAtTheEnd = false;
+    private float _rotationX = 0;
+    private float _rotationY = 0;
+    private float _currentMouseY = 0;
+    private float _currentMouseX = 0;
+    private PlayerAgentController _playerController = null;
 
     public bool CanMove { get => _canMove; set => _canMove = value; }
-    public bool IsAtTheEnd { get => _isAtTheEnd; set => _isAtTheEnd = value; }
+    public bool IsAtTheEnd { get => _isAtTheEnd; set { _isAtTheEnd = value; SetOrientation(); } }
 
     public void Init(PlayerData playerData, Camera camera, CharacterController characterController)
     {
         _playerData = playerData;
         _characterController = characterController;
         _camera = camera;
+        _playerController = PlayerManager.Instance.PlayerController;
     }
 
     public void Update()
@@ -27,18 +33,23 @@ public class IQTELadder : IPlayerState
 
     public void Exit()
     {
-        InputManager.Instance.Direction -= SetOrientation;
+        InputManager.Instance.Direction -= SetDirection;
+        InputManager.Instance.MousePosition -= LookAtMouse;
+        _playerController.CanMove = false;
     }
 
     public void Enter(Collider collider)
     {
-        InputManager.Instance.Direction += SetOrientation;
+        SetOrientation();
+        InputManager.Instance.Direction += SetDirection;
         PlayerManager.Instance.PlayerController.TimeZoom = 0;
+        InputManager.Instance.MousePosition += LookAtMouse;
+        _playerController.CanMove = true;
     }
 
-    private void SetOrientation(float horizontalMouvement, float verticalMouvement)
+    private void SetOrientation()
     {
-        if(_isAtTheEnd == false)
+        if (_isAtTheEnd == false)
         {
             _orientation = _characterController.transform.up;
         }
@@ -46,6 +57,10 @@ public class IQTELadder : IPlayerState
         {
             _orientation = _characterController.transform.forward;
         }
+    }
+
+    private void SetDirection(float horizontalMouvement, float verticalMouvement)
+    {
         _directionHorizontal = horizontalMouvement * _orientation * _playerData.SpeedLadder;
         Move();
     }
@@ -56,6 +71,60 @@ public class IQTELadder : IPlayerState
         {
             Vector3 wantedDirection = _directionHorizontal * _playerData.GlobalSpeed * Time.deltaTime;
             _characterController.Move(wantedDirection);
+        }
+    }
+
+    private void LookAtMouse(float mousePositionX, float mousePositionY)
+    {
+        if (_playerController.CanMove == true && _playerController != null)
+        {
+            if (mousePositionX > 0)
+            {
+                _currentMouseX += Time.deltaTime * _playerData.SpeedToStopCamera;
+                _currentMouseX = Mathf.Clamp(_currentMouseX, -_playerData.StackMovement, _playerData.StackMovement);
+            }
+            else if (mousePositionX < 0)
+            {
+                _currentMouseX -= Time.deltaTime * _playerData.SpeedToStopCamera;
+                _currentMouseX = Mathf.Clamp(_currentMouseX, -_playerData.StackMovement, _playerData.StackMovement);
+            }
+            else if (mousePositionX == 0 && _currentMouseX > 0)
+            {
+                _currentMouseX -= Time.deltaTime * _playerData.SpeedToStopCamera;
+                _currentMouseX = Mathf.Clamp(_currentMouseX, 0, _playerData.StackMovement);
+            }
+            else if (mousePositionX == 0 && _currentMouseX < 0)
+            {
+                _currentMouseX += Time.deltaTime * _playerData.SpeedToStopCamera;
+                _currentMouseX = Mathf.Clamp(_currentMouseX, -_playerData.StackMovement, 0);
+            }
+            if (mousePositionY > 0)
+            {
+                _currentMouseY += Time.deltaTime * _playerData.SpeedToStopCamera;
+                _currentMouseY = Mathf.Clamp(_currentMouseY, -_playerData.StackMovement, _playerData.StackMovement);
+            }
+            else if (mousePositionY < 0)
+            {
+                _currentMouseY -= Time.deltaTime * _playerData.SpeedToStopCamera;
+                _currentMouseY = Mathf.Clamp(_currentMouseY, -_playerData.StackMovement, _playerData.StackMovement);
+            }
+            else if (mousePositionY == 0 && _currentMouseY > 0)
+            {
+                _currentMouseY -= Time.deltaTime * _playerData.SpeedToStopCamera;
+                _currentMouseY = Mathf.Clamp(_currentMouseY, 0, _playerData.StackMovement);
+            }
+            else if (mousePositionY == 0 && _currentMouseY < 0)
+            {
+                _currentMouseY += Time.deltaTime * _playerData.SpeedToStopCamera;
+                _currentMouseY = Mathf.Clamp(_currentMouseY, -_playerData.StackMovement, 0);
+            }
+            _rotationY = _playerController.gameObject.transform.localEulerAngles.y;
+            _rotationX += _currentMouseY + mousePositionY * _playerData.SensitivityMouseX;
+            _rotationY += _currentMouseX + mousePositionX * _playerData.SensitivityMouseY;
+            _rotationY = Mathf.Clamp(_rotationY, _playerData.LadderMinAngleY, _playerData.LadderMinAngleY + _playerData.LadderAngleY);
+            _rotationX = Mathf.Clamp(_rotationX, -_playerData.AngleX, _playerData.AngleX);
+            _playerController.gameObject.transform.localEulerAngles = new Vector3(_playerController.gameObject.transform.rotation.eulerAngles.x, _rotationY, 0);
+            _camera.transform.localEulerAngles = new Vector3(-_rotationX, 0, 0);
         }
     }
 }
