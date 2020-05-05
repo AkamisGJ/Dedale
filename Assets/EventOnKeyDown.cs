cca2e2eee193341bc4cc4f7800d468053e4bb81c
BenPoch <46890326+BenPoch@users.noreply.github.com>
2020-05-05 14:43:57 +0200

    Add files + script event key

---
+++ Assets/EventOnKeyDown.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class scriptkeydialogue : MonoBehaviour
+{
+
+[SerializeField] private UnityEvent _unityEvent = null;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            _unityEvent.Invoke();
+        }
+    }
+}
---
+++ Assets/scriptkeydialogue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventOnKeyDown : MonoBehaviour
+{
+
+public KeyCode m_key;
+[SerializeField] private UnityEvent _unityEvent = null;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("m_key"))
+        {
+            _unityEvent.Invoke();
+        }
+    }
+}
