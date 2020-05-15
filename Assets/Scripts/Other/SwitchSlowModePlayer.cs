6faef2395a963d643843837788cbf2443a5106b1
ValorisFR <valoris@hotmail.fr>
2020-05-15 13:18:55 +0200

    add Slow Mode complete

---
+++ Assets/Scripts/Other/SwitchSlowModePlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SwitchSlowModePlayer : MonoBehaviour
+{
+    [SerializeField] private bool _isSlow = false;
+    private PlayerAgentController _playerAgentController = null;
+
+    private void Start()
+    {
+        _playerAgentController = PlayerManager.Instance.PlayerController;
+    }
+
+    public void SwitchSlowMode()
+    {
+        _playerAgentController.IsSlow = _isSlow;
+    }
+}
