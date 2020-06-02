9013b2cddf790809c5cd9994dade531e19a717df
Akamis <mrtetex@gmail.com>
2020-06-02 11:04:45 +0200

    Fix bug with Salle Opération

--- Assets/Scripts/States/PlayerStates/IMouvement.cs
+++ Assets/Scripts/States/PlayerStates/IMouvement.cs
@@ -652,7 +652,7 @@ public class IMouvement : IPlayerState
     private void FootStepSpeed(Vector3 realMove)
     {
         float speedPlayer = new Vector3(realMove.x, 0, realMove.z).magnitude;
-        Debug.Log(_playerData.TimeStep / Mathf.Log(1.25f + speedPlayer, 2));
+        //Debug.Log(_playerData.TimeStep / Mathf.Log(1.25f + speedPlayer, 2));
         if(speedPlayer > 0 && _currentTimeFootStepPlayer > _playerData.TimeStep / Mathf.Log(1.25f + speedPlayer,2))
         {
             SelectFootStepAndPlay();
